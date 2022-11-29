ce248c2f80459c8cf4edf9426bc7b9641eebeb36
Tapio-adro <terentuk4321@gmail.com>
2022-11-29 09:52:00 +0200

    Minor change

--- src/views/HomeScreen.vue
+++ src/views/HomeScreen.vue
@@ -17,7 +17,7 @@
       <div class="menu_container">
         <div class="menu">
           <router-link id="quick_game" class="router_link" to="/game" @click="runQuickGame">Quick Game</router-link>
-          <router-link id="set_own_game" class="router_link" to="/options">Set own Game</router-link>
+          <router-link id="set_own_game" class="router_link" to="/options">Custom game</router-link>
         </div>
       </div>
     </div>
