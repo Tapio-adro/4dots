d1c2a99e493c0eff46cf03ce5ec171e4d9c1f03b
Tapio-adro <terentuk4321@gmail.com>
2023-04-20 08:10:22 +0200

    Small change for inverted colors

--- public/scripts/game scripts/gameFeatures.js
+++ public/scripts/game scripts/gameFeatures.js
@@ -95,44 +95,39 @@ gameFeatures.getTeamColors = function (amount) {
   let colorFraction = 345 / amount;
   let huesArray = [];
   for (let i = 0; i < amount; i++) {
-      huesArray.push(Math.floor(colorFraction * i));
+    huesArray.push(Math.floor(colorFraction * i));
   }
   let rgbArray = [];
   let rgbInvertedArray = [];
   let s = 100, l = 70;
   for (let hueColor of huesArray) {
-      let rgbColor = HSLToRGB(hueColor, s, l);
-      rgbColor = rgbColor.map(num => Math.floor(num)).join(', ');
-      rgbArray.push(rgbColor)
+    let rgbColor = HSLToRGB(hueColor, s, l);
+    rgbColor = rgbColor.map(num => Math.floor(num)).join(', ');
+    rgbArray.push(rgbColor)
 
-      let hslInverted = invertHSL(hueColor, 70, 70);
-      let rgbInvertedColor = HSLToRGB(...hslInverted);
-      rgbInvertedColor = rgbInvertedColor.map(num => Math.floor(num)).join(', ');
-      rgbInvertedArray.push(rgbInvertedColor)
+    let hslInverted = invertHSL(hueColor, 70, 70);
+    let rgbInvertedColor = HSLToRGB(...hslInverted);
+    rgbInvertedColor = rgbInvertedColor.map(num => Math.floor(num)).join(', ');
+    rgbInvertedArray.push(rgbInvertedColor)
   }
   let colorsArray = [];
   for (let key in rgbArray) {
-      let color = rgbArray[key];
-      colorsArray.push([color, rgbInvertedArray[key]]);
+    let color = rgbArray[key];
+    colorsArray.push([color, rgbInvertedArray[key]]);
   }
   return colorsArray;
 
   function HSLToRGB(h, s, l) {
-      s /= 100;
-      l /= 100;
-      const k = n => (n + h / 30) % 12;
-      const a = s * Math.min(l, 1 - l);
-      const f = n =>
-        l - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
-      return [255 * f(0), 255 * f(8), 255 * f(4)];
+    s /= 100;
+    l /= 100;
+    const k = n => (n + h / 30) % 12;
+    const a = s * Math.min(l, 1 - l);
+    const f = n =>
+      l - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
+    return [255 * f(0), 255 * f(8), 255 * f(4)];
   };
   function invertHSL (h, s, l) {
-      if (h < 180) {
-          h += 180;
-      } else {
-          h -= 180;
-      }
-      return [h, s, l];
+    return [(h + 180) % 360, s, l - 20];
   }
 }
 
